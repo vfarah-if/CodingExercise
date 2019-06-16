c2e6bb8379cd27ab5777db4d68114f626dbe9a23
vfarah-if <sefrica@live.com>
2019-06-16 19:50:43 +0100

    Added failing test for Verify Employee exists

--- src/Exercise.Domain/Bookings/BookingService.cs
+++ src/Exercise.Domain/Bookings/BookingService.cs
@@ -12,10 +12,11 @@ namespace Exercise.Domain.Bookings
     ///     1. Check out date must be at least one day after the check in date (Done).
     ///     2. Validate if the hotel exists and room type is provided by the hotel (Done).    
     ///     3. Verify if booking is allowed according to the booking policies defined, if any. See Booking Policy Service for more details.
-    ///     4. Booking should only be allowed if there is at least one room type available during the whole booking period.
-    ///     5. Keep track of all bookings. E.g. If hotel has 5 standard rooms, we should have no more than 5 bookings in the same day.
-    ///     6. Hotel rooms can be booked many times as long as there are no conflicts with the dates.
-    ///     7. Return booking confirmation to the employee or error otherwise (exceptions can also be used).
+    ///     4. Verify Employee exists (Added)
+    ///     5. Booking should only be allowed if there is at least one room type available during the whole booking period.
+    ///     6. Keep track of all bookings. E.g. If hotel has 5 standard rooms, we should have no more than 5 bookings in the same day.
+    ///     7. Hotel rooms can be booked many times as long as there are no conflicts with the dates.
+    ///     8. Return booking confirmation to the employee or error otherwise (exceptions can also be used).
     /// </remarks>
     public class BookingService
     {
@@ -51,6 +52,7 @@ namespace Exercise.Domain.Bookings
             {
                 return new BookingStatus(startDate: checkIn, endDate: checkOut, errors: HotelNotFound);
             }
+
 
             return new BookingStatus(startDate: checkIn, endDate: checkOut, hotel: hotel, errors: "TODO: Keep test failing for valid reasons");
         }
