9d84b3fdba05a26b6ac2e75b43b4783febff0493
vfarah-if <sefrica@live.com>
2019-06-16 19:24:24 +0100

    Validate if the hotel exists and room type is provided by the hotel

--- src/Exercise.Domain/Bookings/BookingService.cs
+++ src/Exercise.Domain/Bookings/BookingService.cs
@@ -9,7 +9,7 @@ namespace Exercise.Domain.Bookings
     /// </summary>
     /// <remarks>
     ///     1. Check out date must be at least one day after the check in date (Done).
-    ///     2. Validate if the hotel exists and room type is provided by the hotel.
+    ///     2. Validate if the hotel exists and room type is provided by the hotel (Done).    
     ///     3. Verify if booking is allowed according to the booking policies defined, if any. See Booking Policy Service for more details.
     ///     4. Booking should only be allowed if there is at least one room type available during the whole booking period.
     ///     5. Keep track of all bookings. E.g. If hotel has 5 standard rooms, we should have no more than 5 bookings in the same day.
@@ -18,7 +18,7 @@ namespace Exercise.Domain.Bookings
     /// </remarks>
     public class BookingService
     {
-        private const int TwentyFourHours = 24;
+        private const int OneDay = 1;
         private readonly IBookingPolicyService _bookingPolicyService;
         private readonly ICompanyService _companyService;
         private readonly IHotelService _hotelService;
@@ -40,12 +40,18 @@ namespace Exercise.Domain.Bookings
                 return new BookingStatus(startDate: checkIn, endDate: checkOut, errors: "Check-in date can not be less than or equal to check-out date.");
             }
 
-            if (checkOut.Subtract(checkIn).Hours < TwentyFourHours)
+            if (checkOut.Subtract(checkIn).Days < OneDay)
             {
-                return new BookingStatus(startDate: checkIn, endDate: checkOut, errors: "Check-out must be at least 24 hours after Check-in date.");
+                return new BookingStatus(startDate: checkIn, endDate: checkOut, errors: "Check-out must be at least 1 day after Check-in date.");
             }
 
-            return new BookingStatus(startDate: checkIn, endDate: checkOut);
+            var hotel = _hotelService.FindHotelBy(hotelId);
+            if (hotel == null)
+            {
+                return new BookingStatus(startDate: checkIn, endDate: checkOut, errors: "Hotel does not exist.");
+            }
+
+            return new BookingStatus(startDate: checkIn, endDate: checkOut, hotel: hotel);
         }
     }
 }
--- tests/Exercise.Domain.Tests/UnitTests/Bookings/BookingServiceShould.cs
+++ tests/Exercise.Domain.Tests/UnitTests/Bookings/BookingServiceShould.cs
@@ -18,12 +18,22 @@ namespace Exercise.Domain.Tests.UnitTests.Bookings
         private Guid _employeeId;
         private Guid _hotelId;
         private Guid _roomType;
+        private Hotel _hotelExistsResponse;
+        private DateTime _checkIn;
+        private DateTime _checkout;
 
         public BookingServiceShould()
         {
+            _checkIn = DateTime.Now;
+            _checkout = DateTime.Now.AddDays(1);
+
             _bookingServiceMock = new Mock<IBookingPolicyService>();
             _companyServiceMock = new Mock<ICompanyService>();
+
             _hotelServiceMock = new Mock<IHotelService>();
+            _hotelId = Guid.NewGuid();
+            
+            _hotelServiceMock.Setup(x => x.FindHotelBy(_hotelId)).Returns(() => _hotelExistsResponse);
 
             _bookingService = new BookingService(
                 _bookingServiceMock.Object, 
@@ -35,8 +45,8 @@ namespace Exercise.Domain.Tests.UnitTests.Bookings
         [Fact]
         public void VerifyCheckoutDateIsNotLessThanOrEqual()
         {
-            DateTime checkIn = DateTime.Now;
-            DateTime checkout = DateTime.Now;
+            var checkIn = DateTime.Now;
+            var checkout = DateTime.Now;
 
             var actual = _bookingService.Book(_employeeId, _hotelId, _roomType, checkIn, checkout);
 
@@ -46,16 +56,28 @@ namespace Exercise.Domain.Tests.UnitTests.Bookings
         }
 
         [Fact]
-        public void VerifyCheckoutDateIsGreaterThanOrEqualsTo24Hours()
+        public void VerifyCheckoutDateIsGreaterThanOrEqualsTo1Day()
         {
-            DateTime checkIn = DateTime.Now;
-            DateTime checkout = DateTime.Now.AddHours(23);
+            var checkIn = DateTime.Now;
+            var checkout = DateTime.Now.AddHours(23);
 
             var actual = _bookingService.Book(_employeeId, _hotelId, _roomType, checkIn, checkout);
 
             actual.IsBooked.Should().BeFalse();
             actual.Errors.Length.Should().Be(1);
-            actual.Errors.First().Should().Be("Check-out must be at least 24 hours after Check-in date.");
+            actual.Errors.First().Should().Be("Check-out must be at least 1 day after Check-in date.");
+        }
+
+        [Fact]
+        public void NotAllowBookingWhenHotelDoesNotExist()
+        {
+            _hotelExistsResponse = null;
+
+            var actual = _bookingService.Book(_employeeId, _hotelId, _roomType, _checkIn, _checkout);
+
+            actual.IsBooked.Should().BeFalse();
+            actual.Errors.Length.Should().Be(1);
+            actual.Errors.First().Should().Be("Hotel does not exist.");
         }
     }
 }
