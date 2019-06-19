3d0feb76ae16df87b4e30184fb84dad01fa28607
vfarah-if <vincent.farah@codurance.com>
2019-06-19 21:50:18 +0100

    Reafactored code to fix small issues

--- src/Exercise.Domain/BerlinClockConverter.cs
+++ src/Exercise.Domain/BerlinClockConverter.cs
@@ -6,30 +6,39 @@ namespace Exercise.Domain
 {
     public class BerlinClockConverter
     {
+        private const string OffLightRowOfFour = "OOOO";
+        private const string RedLight = "R";
+        private const string OffLight = "O";
+
         public string Convert(string time)
+        {
+            ValidateTimeFormat(time);
+
+            var timeParts = time.Split(':');
+            var seconds = System.Convert.ToInt16(timeParts[2]);
+            var hours = System.Convert.ToInt16(timeParts[0]);
+            var result = new StringBuilder();
+            result.AppendLine(IsEven(seconds) ? RedLight : OffLight);
+            result.AppendLine();
+            result.AppendLine(GetFirstRow(hours));
+            return result.ToString();
+        }
+
+        private static void ValidateTimeFormat(string time)
         {
             Regex regex = new Regex(@"^(\d\d:\d\d:\d\d)$");
             if (!regex.IsMatch(time))
             {
                 throw new NotSupportedException("Time should be in the expected 'hh:mm:ss' format");
             }
-
-            var timeParts = time.Split(':');
-            var seconds = System.Convert.ToInt16(timeParts[2]);
-            var hours = System.Convert.ToInt16(timeParts[0]);
-            var result = new StringBuilder();
-            result.AppendLine(IsEven(seconds) ? "R" : "O");
-            result.AppendLine();
-            result.AppendLine(GetFirstRow(hours));
-            return result.ToString();
         }
 
         private string GetFirstRow(short hours)
         {
-            var result = new StringBuilder("OOOO");
+            var result = new StringBuilder(OffLightRowOfFour);
             for (int i = 0; i < hours/5; i++)
             {
-                result.Replace("O", "R", i, 1);
+                result.Replace(OffLight, RedLight, i, 1);
             }
 
             return result.ToString();
