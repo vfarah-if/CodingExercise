07af4c9df4f95ae6aa7391096848839f31b7ebcd
vfarah-if <vincent.farah@codurance.com>
2019-12-08 03:57:59 +0000

    Added my favourite XML to get content in more intuituve format

--- src/Exercise.Domain/RuleBookPDFParsor.cs
+++ src/Exercise.Domain/RuleBookPDFParsor.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 using SautinSoft;
 
 namespace Exercise.Domain
@@ -7,14 +8,42 @@ namespace Exercise.Domain
     {
         public string ToHtml(string pdfFilePath)
         {
+            return ValidateAndOpenPdf(pdfFilePath).ToHtml();
+        }
+
+        public string ToText(string pdfFilePath)
+        {
+            return ValidateAndOpenPdf(pdfFilePath).ToText();
+        }
+
+        public string ToXml(string pdfFilePath)
+        {
+
+            return ValidateAndOpenPdf(pdfFilePath).ToXml();
+        }
+
+        public byte[] ToWord(string pdfFilePath)
+        {
+            return ValidateAndOpenPdf(pdfFilePath).ToWord();
+        }
+
+        public string ToWordAsString(string pdfFilePath, string encodingName = "utf-8")
+        {
+            var result = ToWord(pdfFilePath);
+            return Encoding.GetEncoding(encodingName).GetString(result);
+        }
+
+        private static PdfFocus ValidateAndOpenPdf(string pdfFilePath)
+        {
             if (!File.Exists(pdfFilePath))
             {
                 throw new FileNotFoundException("Unable to locate Rulebook", pdfFilePath);
             }
 
-            var pdfFocus = new PdfFocus();
-            pdfFocus.OpenPdf(pdfFilePath);
-            return pdfFocus.ToHtml();
+            var result = new PdfFocus();
+            result.OpenPdf(pdfFilePath);
+            return result;
         }
+
     }
 }
--- tests/Exercise.Domain.Tests/UnitTests/RuleBookPDFParsorShould.cs
+++ tests/Exercise.Domain.Tests/UnitTests/RuleBookPDFParsorShould.cs
@@ -7,14 +7,51 @@ namespace Exercise.Domain.Tests.UnitTests
 {
     public class RuleBookPDFParsorShould
     {
+        private const string tableOfContentsPdfPath = "TestData\\Table of Contents.pdf";
         private RuleBookPDFParsor ruleBookPDFParsor;
 
-        [Fact]
-        public void ExtractPDFDataAsHtmlInorderToParseContentIntoKnownModel()
+        public RuleBookPDFParsorShould()
         {
             ruleBookPDFParsor = new RuleBookPDFParsor();
-            string testFile = Path.Combine(Environment.CurrentDirectory,"TestData\\Table of Contents.pdf");
+        }
+
+        [Fact]
+        public void ExtractPDFDataAsHtml()
+        {
+            string testFile = Path.Combine(Environment.CurrentDirectory, tableOfContentsPdfPath);
+
             var result = ruleBookPDFParsor.ToHtml(testFile);
+
+            result.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public void ExtractPDFDataAsText()
+        {
+            string testFile = Path.Combine(Environment.CurrentDirectory, tableOfContentsPdfPath);
+
+            var result = ruleBookPDFParsor.ToText(testFile);
+
+            result.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public void ExtractPDFDataAsXml()
+        {
+            string testFile = Path.Combine(Environment.CurrentDirectory, tableOfContentsPdfPath);
+
+            var result = ruleBookPDFParsor.ToXml(testFile);
+
+            result.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public void ExtractPDFDataAsWordText()
+        {
+            string testFile = Path.Combine(Environment.CurrentDirectory, tableOfContentsPdfPath);
+
+            var result = ruleBookPDFParsor.ToWordAsString(testFile);
+
             result.Should().NotBeNullOrEmpty();
         }
     }
